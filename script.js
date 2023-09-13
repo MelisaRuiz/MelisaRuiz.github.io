54a6deab542e03b17ae9b20fc053f007a3fc7cc2
melisaruiz <144286838+MelisaRuiz@users.noreply.github.com>
2023-09-12 23:02:00 -0400

    Add files via upload
    4commit

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const SECOND = document.getElementById('calsec');
 const FIRST = document.getElementById('calpri');
+const SECOND = document.getElementById('calsec');
 const THIRD = document.getElementById('calter');
-const PUSH = document.getElementById('ingresar');
 const mal = document.getElementById('mal');
+const PUSH = document.getElementById('ingresar');
 
 PUSH.addEventListener('click', () => {
     const CALCU = document.getElementById('valor').value;
-
     if (CALCU >0){
-        let calpri = calcFlujo(CALCU);
-        let volumen = calpri / 24;
-        let medio = volumen * 1.5;
+        let calpri = calc(CALCU);
+        let volumen = CALCU;
+        let medio = volumen;
         FIRST.innerHTML = calpri + ' cc';
-        THIRD.innerHTML = volumen + ' cc/hr';
-        SECOND.innerHTML = medio + ' cc/hr: ' +' (m+m/2)';
+        THIRD.innerHTML = volumen /24 + ' cc/hr';
+        SECOND.innerHTML = medio * 1.5 +' cc/hr: ' +' (m+m/2)';
         SECOND.style.display = 'block';
         THIRD.style.display = 'block';
         FIRST.style.display = 'block';
@@ -24,34 +23,38 @@ PUSH.addEventListener('click', () => {
         THIRD.style.display = 'none';
         FIRST.style.display = 'none';
         }
-        
-    function calcFlujo(CALCU){
-        if (CALCU >= 1 && CALCU <= 10){
-           respo = CALCU * 100 
-            }
-        if (CALCU >= 11 && CALCU <= 20){
-            respo = (CALCU - 10) * 50 + (10 * 100);
-            }
-        if (CALCU >= 21 && CALCU <= 29){ 
-            resp = (CALCU - 20) * 20 + (9 * 50) + (10 * 100);
-            }
-        if (CALCU >= 30 ) {
-            supfi = ((CALCU * 4) + 7) / (CALCU + 90);
-            respo = "Por 1500" + supfi *1500;
-            resp = "Por 2000" + supfi * 2000;
-            }
-        return Math.round(resp);
+function calc(CALCU){
+    resp=1
+    exe=1
+    valor=CALCU
+    if (valor > 29 ) {
+        respi = (valor * 4 + 7)/(valor + 90);
+        resp = 'Por 1500 y ' + (respi *1500 )+ 'Por 2000' + (respi * 2000);
         }
-});
+    if (CALCU >= 1 && CALCU < 11){
+        resp = valor * 100 
+        }
+    if (CALCU > 10 && valor < 21){
+        exe = ( valor - 10); 
+        resp= (1000) + (exe * 50);
+        }
+    if (CALCU > 20 && valor < 30){ 
+        exe = (valor - 20); 
+        resp= (1500) + (450) + (exe * 20);
+        }
+    return Math.round(resp);
+    }
 let textbtn = document.getElementById('textbtn');
 let texto = document.getElementById('texto');
 textbtn.addEventListener('click', letex)
 function letex() {
     texto.classList.toggle('show');
-}
+
+    }
 let textbtne = document.getElementById('textbtne');
 let texte = document.getElementById('texte');
 textbtne.addEventListener('click', letexi)
 function letexi() {
     texte.classList.toggle('other');
-}
+    }
+});
