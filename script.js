97dbc4d2d8e9a8cbcf8b327eb4850310d4e24677
melisaruiz <144286838+MelisaRuiz@users.noreply.github.com>
2023-09-15 22:03:43 -0400

    Finish Commit

--- script.js
+++ script.js
@@ -1,107 +1,76 @@
-const mal = document.getElementById('mal');
-const FIRST = document.getElementById('calpri');
-const SECRETO = document.getElementById('ocult');
-const SECRETITO = document.getElementById('oculto');
-const SECOND = document.getElementById('calsec');
-const THIRD = document.getElementById('calter');
-const PUSH = document.getElementById('ingresar');
 
-PUSH.addEventListener('click', () => {
-    const CALCU = document.getElementById('valor').value;
-    if (CALCU>0){
-        SECOND.style.display = 'inline';
-        THIRD.style.display = 'inline';
-        FIRST.style.display = 'inline';
-        SECRETITO.style.display = 'inline';
-        SECRETO.style.display = 'inline';
+const BUTTON = document.getElementById("guess-button");
+const MAL = document.getElementById('mal');
+let cant = 6;
+BUTTON.disabled = true;
+
+const API = "https://random-word-api.herokuapp.com/word?length=5"
+
+fetch(API)
+    .then((response)=> response.json())
+    .then((response)=> {
+        sorteo = response[0].toUpperCase();
+        BUTTON.disabled = false;
+        console.log (sorteo);
+    })
+    .catch((err)=>{
+        console.log(err);
+        let random = Math.floor(random() * LIBRERO.length);
+        sorteo = LIBRERO[random];
+        BUTTON.disabled = false;
+    });
+
+BUTTON.addEventListener("click", ()=>{
+    const INTENTO = leerIntento();
+    const GRID = document.getElementById("grid");
+    let row = document.createElement("div");
+    row.className = "row";
+    if (INTENTO.length !=5){
+        MAL.style.display = 'block';
+        armarLetra.style.display = 'none';
+        }
+    if (sorteo == INTENTO){
+        terminar("<h1>GANASTE! ;)</h1>");
+        BUTTON.disabled = true;
+        MAL.style.display = 'none';
         }
     else {
-        mal.style.display = 'visible';
-        SECOND.style.display ='none';
-        THIRD.style.display = 'none';
-        FIRST.style.display = 'none';
-        SECRETITO.style.display = 'none';
-        SECRETO.style.display = 'none';
+    BUTTON.disabled = false;
+        for (let i in sorteo){
+            if (sorteo[i] === INTENTO[i]){
+                let cuadroLetra = armarLetra(INTENTO[i], "green");
+                row.appendChild(cuadroLetra);
+                }
+            else if (sorteo.includes(INTENTO[i])){
+                let cuadroLetra = armarLetra(INTENTO[i], "#f3c237");
+                row.appendChild(cuadroLetra);
+                }
+            else {
+                let cuadroLetra = armarLetra(INTENTO[i], "gray");
+                row.appendChild(cuadroLetra);
+                } 
+            }
+        cant--;
         }
-    
-    if (CALCU>0 && CALCU < 30){
-        let calpri = calc(CALCU);
-        FIRST.innerHTML = calpri + ' cc';
-        THIRD.innerHTML = calpri/24 + ' cc/hr';
-        SECOND.innerHTML = (calpri/24) * 1.5 +' cc/hr';
-        SECRETITO.style.display = 'none';
-        SECRETO.style.display = 'none';
-        }
-    if (CALCU> 29){
-        let ocult= lala(CALCU);
-        let oculto= rita(CALCU);
-        SECRETO.innerHTML = ("Por 1500 = ") + ocult + (' cc');
-        SECRETITO.innerHTML = ("Por 2000 = ") + oculto + (' cc');
-        SECOND.style.display ='none';
-        THIRD.style.display = 'none';
-        FIRST.style.display = 'none';
-        } 
-    
-function calc(CALCU){
-    resa=0
-    exe=0
-    valor=CALCU
-    if (CALCU >= 1 && CALCU < 11){
-        resa = valor * 100 
-        }
-    if (CALCU > 10 && valor < 21){
-        exe = ( valor - 10); 
-        resa= (1000) + (exe * 50);
-        }
-    if (CALCU > 20 && valor < 30){ 
-        exe = (valor - 20); 
-        resa= (1500) + (450) + (exe * 20);
-        }
-    return Math.round(resa);
+    GRID.appendChild(row)
+    MAL.style.display = 'none';
+    if (cant == 0){
+        terminar("<h1>PERDISTE! ;(</h1>");
+        BUTTON.disabled = true;
+    }
+function leerIntento() {
+    return document.getElementById("guess-input").value.toUpperCase();
+    }
+function terminar (mensaje) {
+    document.getElementById("mensaje").innerHTML = mensaje;
+    }
+ function armarLetra(letra, color){
+    let span = document.createElement("span");
+    span.className = 'letter';
+    span.innerHTML = letra;
+    span.style.backgroundColor = color;
+    return span
     }
 
-
-function lala(CALCU){
-    valor=CALCU
-    resp=0
-    exe=0
-    exe= (valor * 4 + 7)/(valor + 90);
-    resp= exe * 1500;
-    return Math.round(resp);
-    }
-function rita(CALCU){
-    valor=CALCU
-    exe=0
-    respi= 0;
-    exe= (valor * 4 + 7)/(valor + 90);
-    respi= exe * 2000;
-    return Math.round(respi);
-    }
-
-let textbtn = document.getElementById('textbtn');
-let texto = document.getElementById('texto');
-textbtn.addEventListener('click', letex);
-function letex() {
-    texto.classList.toggle('show');
-    }
-    if (CALCU>0 && CALCU<30){
-        textbtn.style.color = 'blue';
-        }
-    else {
-        textbtn.style.color = 'black';
-        }
-
-        
-let textbtne = document.getElementById('textbtne');
-let texte = document.getElementById('texte');
-textbtne.addEventListener('click', letexi);
-function letexi() {
-    texte.classList.toggle('other');
-    }
-    if (CALCU>0 && CALCU>29){
-        textbtne.style.color = 'blue';
-        }
-    else {
-        textbtne.style.color = 'black';
-        }
+console.log (sorteo); 
 });
